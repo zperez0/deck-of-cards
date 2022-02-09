08a93af65950ebafe23ee550d442324d3fc7dd0f
Yazmin Perez <zoeperez333@gmail.com>
2022-02-09 12:57:15 -0800

    create heart list in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,35 @@
 $(document).ready(function () {
-  const clubSuit = ["clubs"];
-  const clubRanks = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
+  const clubSuit = ['clubs'];
+  const clubRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 clubSuit.forEach(function(suit) {
   clubRanks.forEach(function(ranks) {
-    $("#myList").append("<li>" + ranks + " of " + suit + "</li>");
+    $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
   });
 });
     const diamondSuit = ["diamonds"];
-    const diamondRanks = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
+    const diamondRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 
 diamondSuit.forEach(function(suit) {
     diamondRanks.forEach(function(ranks) {
-    $("#myList").append("<li>" + ranks + " of " + suit + "</li>");
+    $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
   });
 });
 
 const spadeSuit = ['spade'];
-const spadeRanks = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
+const spadeRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 
 spadeSuit.forEach(function(suit){
   spadeRanks.forEach(function(ranks){
-  $("#myList").append("<li>" + ranks + " of " + suit + "</li>");
+    $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
   });
 });
 
+const heartSuit = ['heart'];
+const heartRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 
+heartSuit.forEach(function(suit){
+  heartRanks.forEach(function(ranks){
+    $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
 });
-
-
-// create an empty array and push the other two arrays. need to slice or split?
-
-// use concat(clubSuit) & push?
-
-// use index to find positon?
-
-// clubRanks.push(club);
-//     console.log(clubRanks);
-
-// clubSuit.forEach(function(club) {
-//   emptyArray.push(clubRanks.concat(club));
-//   console.log(emptyArray);
+});
+});
