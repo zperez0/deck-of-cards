05a87384d42f6fff53ca686d8e6fed5a730549cf
Yazmin Perez <zoeperez333@gmail.com>
2022-02-09 13:00:51 -0800

    correct formatting in html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
   const clubSuit = ['clubs'];
   const clubRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
-clubSuit.forEach(function(suit) {
-  clubRanks.forEach(function(ranks) {
+  clubSuit.forEach(function (suit) {
+    clubRanks.forEach(function (ranks) {
     $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
+    });
   });
-});
-    const diamondSuit = ["diamonds"];
-    const diamondRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
+  const diamondSuit = ['diamonds'];
+  const diamondRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 
-diamondSuit.forEach(function(suit) {
-    diamondRanks.forEach(function(ranks) {
+  diamondSuit.forEach(function (suit) {
+    diamondRanks.forEach(function (ranks) {
     $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
+    });
   });
-});
 
-const spadeSuit = ['spade'];
-const spadeRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
+  const spadeSuit = ['spade'];
+  const spadeRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 
-spadeSuit.forEach(function(suit){
-  spadeRanks.forEach(function(ranks){
+  spadeSuit.forEach(function (suit) {
+    spadeRanks.forEach(function (ranks) {
     $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
+    });
   });
-});
 
-const heartSuit = ['heart'];
-const heartRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
+  const heartSuit = ['heart'];
+  const heartRanks = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king'];
 
-heartSuit.forEach(function(suit){
-  heartRanks.forEach(function(ranks){
+  heartSuit.forEach(function (suit) {
+    heartRanks.forEach(function (ranks) {
     $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
-});
-});
+    });
+  });
 });
