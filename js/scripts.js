87507ea551aa7433b9c7c319a032ac59f8fc5790
Yazmin Perez <zoeperez333@gmail.com>
2022-02-09 12:35:56 -0800

    create club list in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
   
-  
-}
+  const clubSuit = ['club'];
+  const clubRanks = ['ace','2','3','4','5','6','7','8','9','10', 'jack','queen', 'king'];
+
+  clubSuit.forEach(function(suit) {
+    clubRanks.forEach(function(ranks) {
+    $('#myList').append('<li>' + ranks + ' of ' + suit + '</li>');
+    });
+
+  const 
+  });
+});
+
+
+// create an empty array and push the other two arrays. need to slice or split?
+
+// use concat(clubSuit) & push?
+
+// use index to find positon?
+
+// clubRanks.push(club);
+//     console.log(clubRanks);
+
+// clubSuit.forEach(function(club) {
+//   emptyArray.push(clubRanks.concat(club));
+//   console.log(emptyArray);
+
